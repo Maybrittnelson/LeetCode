8dc915217097c5002e5cbbb11adb655d50612c95
Gary Ge <848236504@qq.com>
2019-08-18 14:47:03 +0800

    90、131

--- LeetCode-In-Java/src/medium/backtracing/LeetCode_131.java
+++ LeetCode-In-Java/src/medium/backtracing/LeetCode_131.java
@@ -1,4 +1,40 @@
 package medium.backtracing;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeetCode_131 {
+
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), s, 0);
+        return result;
+    }
+
+    public void backtrack(List<List<String>> result, List<String> tmpList, String s, int start) {
+        if (start == s.length()) {
+            result.add(new ArrayList<>(tmpList));
+        } else {
+            for (int i = start; i < s.length(); i++) {
+                if (palindrome(s, start, i)) {
+                    tmpList.add(s.substring(start, i + 1));
+                    backtrack(result, tmpList, s, i + 1);
+                    tmpList.remove(tmpList.size() - 1);
+                }
+            }
+        }
+    }
+
+    public boolean palindrome(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start++) != s.charAt(end--)) return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        LeetCode_131 leetCode_131 = new LeetCode_131();
+        System.out.println(leetCode_131.partition("aab"));
+    }
 }
--- LeetCode-In-Java/src/medium/backtracing/LeetCode_90.java
+++ LeetCode-In-Java/src/medium/backtracing/LeetCode_90.java
@@ -1,4 +1,31 @@
 package medium.backtracing;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class LeetCode_90 {
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(result, new ArrayList<>(), nums, 0);
+        return result;
+    }
+
+    public void backtrack(List<List<Integer>> result, List<Integer> tmpList, int[] nums, int start) {
+        result.add(new ArrayList<>(tmpList));
+        for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i - 1]) continue;
+            tmpList.add(nums[i]);
+            backtrack(result, tmpList, nums, i+1);
+            tmpList.remove(tmpList.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        LeetCode_90 leetCode_90 = new LeetCode_90();
+        List<List<Integer>> lists = leetCode_90.subsetsWithDup(new int[]{1, 2, 2});
+        System.out.println(lists);
+    }
 }
